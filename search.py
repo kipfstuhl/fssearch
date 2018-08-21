5f7fb1efb1714c7664680507e4a26d61a769e99c
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2018-08-21 18:29:44 +0200

    initial commit
    Parsing could be better.
    It lacks a possibility to display further results, not just the first ten.

---
+++ search.py
@@ -0,0 +1,91 @@
+from elasticsearch import Elasticsearch
+import subprocess
+import sys
+
+
+
+es = Elasticsearch(['localhost'])
+
+def print_res(result, index=None):
+    if index is not None:
+        print(i,"Title:\t", result['title'])
+        print("  Description:\t", result['description'])
+        print("  Path: ", result['path'])
+    else:
+        print("Title:\t\t", result['title'])
+        print("Description:\t", result['description'])
+        print("Path: ", result['path'])
+
+
+user_search = input("Search term: ")
+
+req_body = {
+    "query": {
+        "multi_match" : {
+            "query" : user_search,
+            "fields" : ["content", "title", "author"],
+            "fuzziness" : "AUTO"
+        }
+    },
+    "sort": {
+        "_score": {"order": "desc"}
+    },
+    "highlight": {
+        "fields": {
+            "content": {}
+        }
+    }
+}
+
+res2 = es.search(index="test", body=req_body, _source=['file.filename', 'path.real', 'meta.title', 'meta.raw.description'])
+
+# parse results
+interesting = []
+for item in res2['hits']['hits']:
+    source = item['_source']
+    meta = source.get('meta')
+
+    title   = None
+    descr   = None
+    os_path = None
+    if meta is not None:
+        title = meta.get('title')
+        if meta.get('raw') is not None:
+            descr = meta.get('raw').get('description')
+    
+    path  = source.get('path')    
+    if path is not None:
+        os_path = path.get('real')
+    
+    try:
+        temp = {
+            'id' :          item['_id'],
+            'title' :       title,
+            'description' : descr,
+            'path' :        os_path
+        }
+        interesting.append(temp)
+    except Exception as e:
+        pass
+
+# print the interesting parts of the results
+print("Found", res2['hits']['total'], "results")
+print()
+for i, item in enumerate(interesting):
+    print_res(item, i)
+    print()
+
+# ask user for opening a search result
+# give the possibility to choose more than one result
+while True:
+    user_value = input("Type number to open, q to exit\n")
+    try:
+        want = int(user_value)
+    except ValueError:
+        if user_value == "q":
+            sys.exit()
+        else:
+            print("Not a number")
+            
+    subprocess.call(["xdg-open", interesting[want]['path']])
+            
